a4f72d3d7e0f3b367531faab2cd8c787a530aa8a
sage-jordan <sagemjrodan@gmail.com>
2019-10-13 20:40:13 -0400

    made axios.get in BubblePage

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -9,6 +9,12 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/colors`)
+      .then(res => setColorList([res.data]))
+      .catch(err => console.error(err));
+  }, []);
+
   return (
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
